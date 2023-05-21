421d2b17925da6ee8bd115413784f88e6f49407d
Kudese <d.kuprichenkov@gmail.com>
2023-05-21 11:02:59 +0300

    add thunk data

--- src/pages/Twets.jsx
+++ src/pages/Twets.jsx
@@ -1,31 +1,32 @@
-
 import { useEffect } from "react";
 import CardUser from "../component/CardUser";
 import axios from "axios";
+import { useState } from "react";
+import css from './Twets.module.css'
+const Tweets = () => {
+  const [page, setPage] = useState(1);
+  const [dataUsers, setDataUsers] = useState([]);
+  async function userList(page) {
+    const { data } = await axios.get(
+      "https://6469673b03bb12ac208c0c6d.mockapi.io/users",
+      { params: { limit: 3, page: page } }
+    );
+    setDataUsers(data);
+  }
+  useEffect(() => {
+    userList(page);
+  }, [page]);
 
-const data = {
-  "name": "Ida Herman",
-  "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/491.jpg",
-  "tweets": "3",
-  "followers": "1",
-}
-
-const  Tweets = () =>  {
-
-  useEffect(  ()=>{
-   async function  asd (page){
-      const datas = await axios.get('https://6469673b03bb12ac208c0c6d.mockapi.io/users', {params: {
-        limit:10
-      }})
-      console.log(datas)
-      console.log(1)
-      return datas
-    }
-    asd(1)
-  })
-  return (<div> 
-    <CardUser data={data}/>
-  </div>);
+  return (<div >
+    <div className={css.conteiner}>
+      {" "}
+      {dataUsers.map((user) => {
+        return <CardUser key={user.id} data={user} />;
+      })}
+    </div>
+      <div className={css.}></div>
+      </div>
+  );
 };
 
 export default Tweets;
